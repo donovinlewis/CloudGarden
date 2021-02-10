907d539bfa551a4dcc66dd3ce26e35bce22501ae
donovinlewis <donovind1@gmail.com>
2021-02-10 13:48:09 -0500

    Modified order

--- Device/main/comSMS.h
+++ Device/main/comSMS.h
@@ -1,13 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+//Common libraries needed across sections
 #include "Arduino.h"
 
+//Microcontroller Pin Definitions
 const int analogPin = A0;
 
+//Custom Libraries
+#include "sms.h"
 //#include "batmon.h"
 //#include "telem.h"
-#include "sms.h"
+
 
 
 #endif
