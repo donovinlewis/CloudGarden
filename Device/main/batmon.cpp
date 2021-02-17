9a389ad1ea9926f875229ad38ddc615d681ba785
donovinlewis <donovind1@gmail.com>
2021-02-17 18:38:25 -0500

    Changed definitions to simplify

--- Device/main/batmon.cpp
+++ Device/main/batmon.cpp
@@ -8,10 +8,10 @@ batMon::batMon() {
 // Returns a measurement of the voltage of the connected LiIon Polymer battery
 double batMon::getVoltage()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_VCELL_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_VCELL_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2,HIGH); //send stop
+	Wire.requestFrom(BAT_ADDR, (int)2,HIGH); //send stop
     uint16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
     double voltage = combined; //combine registers
     return voltage*0.078125; // //calculate actual value and return in mV
@@ -19,10 +19,10 @@ double batMon::getVoltage()
 
 double batMon::getCurrent()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_CURENT_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_CURENT_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2,HIGH); //send stop
+	Wire.requestFrom(BAT_ADDR, (int)2,HIGH); //send stop
     int16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
     double current = (double)combined*0.0015625/0.01;
     return current;//calculate actual value as 0.0015625 mV/Ohm
@@ -30,10 +30,10 @@ double batMon::getCurrent()
 
 double batMon::getTemperature()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_TEMP_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_TEMP_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2,HIGH); //send stop
+	Wire.requestFrom(BAT_ADDR, (int)2,HIGH); //send stop
     int16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
     double temperature = (double)combined/256;
     return temperature;
@@ -43,10 +43,10 @@ double batMon::getTemperature()
 // as a percentage of the full capacity w/ resolution 1/256%
 double batMon::getSOC()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_REPSOC_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_REPSOC_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2);
+	Wire.requestFrom(BAT_ADDR, (int)2);
     uint16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
     double soc = combined; //combine registers
 	return soc/256; //calculate actual value and return in %
@@ -55,10 +55,10 @@ double batMon::getSOC()
 // RepCap or reported capacity is a filtered version of the AvCap register that prevents large jumps in the reported value caused by changes in the application such as abrupt changes in temperature or load current. 
 double batMon::getCapacity()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_REPCAP_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_REPCAP_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2);
+	Wire.requestFrom(BAT_ADDR, (int)2);
     uint16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
 	double capacity = (double)combined*0.005/0.01;
     return capacity;//calculate actual value as 0.005 mVh/Ohm
@@ -67,10 +67,10 @@ double batMon::getCapacity()
 // The TTE register holds the estimated time to empty for the application under present temperature and load conditions 
 double batMon::getTTE()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_TTE_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_TTE_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2);
+	Wire.requestFrom(BAT_ADDR, (int)2);
     uint16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
 	double capacity = (double)combined*5.625;
     return capacity;//calculate actual value as value*5.625s
@@ -79,10 +79,10 @@ double batMon::getTTE()
 // The TTF register holds the estimated time to full for the application under present conditions. 
 double batMon::getTTF()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_TTF_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_TTF_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2);
+	Wire.requestFrom(BAT_ADDR, (int)2);
     uint16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
 	double capacity = (double)combined*5.625;
     return capacity;//calculate actual value as value*5.625s
@@ -91,10 +91,10 @@ double batMon::getTTF()
 // Status Register (000h) The Status register maintains all flags related to alert thresholds and battery insertion or removal.
 uint16_t batMon::getStatus()
 {
-	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_STATUS_ADDR);
+	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_STAT_ADDR);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MAX1720X_ADDR, (int)2);
+	Wire.requestFrom(BAT_ADDR, (int)2);
     uint16_t combined = Wire.read()|(Wire.read()<<8); // LSB or-ed with MSB
 	return combined;
 }
@@ -102,14 +102,14 @@ uint16_t batMon::getStatus()
 // Reset procedure
 uint8_t batMon::reset()
 {
- 	Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_COMMAND_ADDR);
+ 	Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_COMMAND_ADDR);
 	Wire.write(0x0f);
 	Wire.write(0x00);
     Wire.endTransmission();
     delay(50);
-    Wire.beginTransmission(MAX1720X_ADDR);
-	Wire.write(MAX1720X_CONFIG2_ADDR);
+    Wire.beginTransmission(BAT_ADDR);
+	Wire.write(BAT_COMM2_ADDR);
 	Wire.write(0x01);
 	Wire.write(0x00);
 	return Wire.endTransmission();
--- Device/main/batmon.h
+++ Device/main/batmon.h
@@ -5,22 +5,22 @@
 #include <Wire.h>
 
 // MAX1720X register addresses
-const int MAX1720X_ADDR = 0x36;
-const int MAX1720X_STATUS_ADDR = 0x00; // Contains alert status and chip status
-const int MAX1720X_VCELL_ADDR = 0x09; // Lowest cell voltage of a pack, or the cell voltage for a single cell
-const int MAX1720X_REPSOC_ADDR = 0x06; // Reported state of charge
-const int MAX1720X_REPCAP_ADDR = 0x05; // Reported remaining capacity
-const int MAX1720X_TEMP_ADDR = 0x08; // Temperature
-const int MAX1720X_CURENT_ADDR = 0x0A; // Battery current
-const int MAX1720X_TTE_ADDR = 0x11; // Time to empty
-const int MAX1720X_TTF_ADDR = 0x20; // Time to full
-const int MAX1720X_CAPACITY_ADDR = 0x10; // Full capacity estimation
-const int MAX1720X_VBAT_ADDR = 0xDA; // Battery pack voltage
-const int MAX1720X_AVCELL_ADDR = 0x17; // Battery cycles
-const int MAX1720X_COMMAND_ADDR = 0x60; // Command register
-const int MAX1720X_CONFIG2_ADDR = 0xbb; // Command register
+const int BAT_ADDR = 0x36;
+const int BAT_STAT_ADDR = 0x00;  // Contains alert status and chip status
+const int BAT_VCELL_ADDR = 0x09;   // Lowest cell voltage of a pack, or the cell voltage for a single cell
+const int BAT_REPSOC_ADDR = 0x06;  // Reported state of charge
+const int BAT_REPCAP_ADDR = 0x05;  // Reported remaining capacity
+const int BAT_TEMP_ADDR = 0x08;    // Temperature
+const int BAT_CURENT_ADDR = 0x0A;  // Battery current
+const int BAT_TTE_ADDR = 0x11;     // Time to empty
+const int BAT_TTF_ADDR = 0x20;     // Time to full
+const int BAT_CAPACITY_ADDR = 0x10; // Full capacity estimation
+const int BAT_VBAT_ADDR = 0xDA;    // Battery pack voltage
+const int BAT_AVCELL_ADDR = 0x17;  // Battery cycles
+const int BAT_COMMAND_ADDR = 0x60; // Command register
+const int BAT_COMM2_ADDR = 0xbb;   // Command register
 
-// Class for MAX17201 battery fuel gauges
+// Class for the battery monitoring fuel gauge
 class batMon
 {
 public:
